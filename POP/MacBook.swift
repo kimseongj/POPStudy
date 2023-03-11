cd70d1158504c99ffd7c03617f1f951566f43b59
kimseongj <ksj8277@naver.com>
2023-03-11 11:36:29 +0900

    feat : MacBook 구조체 구현

--- POP/MacBook.swift
+++ POP/MacBook.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct MacBook {
+    let allowedChargeWattPerHour: WattPerHour
+    var battery: WattPerHour
+    let maximumBattery: WattPerHour = 100
+    
+    
+    mutating func chargeBattery(charger: Chargeable) {
+        var needBattery = maximumBattery - battery
+        
+        let maxCharge = charger.convert(chargeableWattPerHour: self.allowedChargeWattPerHour)
+        
+         let time = needBattery / maxCharge
+        
+        print(time)
+    }
+}
